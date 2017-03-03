2b830d77bdce899f2c4d3e1bee08be0b28743a2e
Tifosi Li <tifosi19921030@gmail.com>
2017-03-03 11:36:55 +0800

    Update readme and a missing file.

---
+++ DB_Spider.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import pandas as pd
+ 
+urls = ['https://book.douban.com/subject/6813370/',
+        'https://book.douban.com/subject/1032672/',
+        'https://book.douban.com/subject/1753077/',
+        'https://book.douban.com/subject/1418180/',
+        'https://book.douban.com/subject/3136271/',
+        'https://book.douban.com/subject/1203293/',
+        'https://book.douban.com/subject/24934182/']
+words = []
+ 
+for url in urls:
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'lxml')
+    tags = soup.select('a[class=" tag"]')
+    with open('d:/test.txt','a') as w:
+        for name in tags:
+            words.append(name.get_text().lower())
+            w.write(name.get_text().lower() + ',')
+    time.sleep(3)
+    print('wait...')
+ 
+p = pd.Series(words)
+freq = p.value_counts()
+ 
+with open('d:/test.txt', 'a') as w:
+    w.write('\n' + str(freq))
+ 
+print(freq) 
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Scrapy
+
+### AQI_Spider.py
+
+Extract beijing AQI from *http://aqicn.org/city/beijing/* per hour and save it to MYSQL, meantime read last 4 hours AQI data if the quality goes better or worse, send an e-mail.
+
+### Douban spider
+
+For my girlfriend.
+
+Get what books she like.
+
+##### DB_Spider.py
+
+Go to book's douban website, crawl book's tags and save it in hottags.txt.
+
+##### ANLYSIS_TAGS.py
+
+Anlysis tags frequency and sort.
+
+##### Extract_Book_Tags.py
+
+Get tags from hottags.txt, visit every tag's douban website, sort by rating, pick top 40 books. Go to all of these books page, crawl tags, place book names and tags in a excel.
+
+##### In Excel
+
+Each tag times its frequency as a point and sum every book's tag point. Sort by point, so we have a book list she might like.
+
+##### Find out
+
+She like it. :)
